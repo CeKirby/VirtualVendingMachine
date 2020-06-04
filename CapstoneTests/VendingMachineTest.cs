4d0d70fa77f188e41d7294f1610a26492412ccc2
Don Coco <steeler15@gmail.com>
2020-06-04 15:34:44 -0400

    have feed money and return money

---
+++ CapstoneTests/VendingMachineTest.cs
@@ -0,0 +1,27 @@
+﻿using Capstone;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapstoneTests
+{
+    [TestClass]
+    public class VendingMachineTest
+    {
+        [TestMethod]
+        public void AddMoney()
+        {
+            VendingMachine addMoney = new VendingMachine();
+            decimal expectedReturn = 2m;
+            Assert.AreEqual(expectedReturn, addMoney.FeedMoney(2));
+        }
+        [TestMethod]
+        public void ReturnMoney()
+        {
+            VendingMachine returnMoney = new VendingMachine();
+            decimal expectedReturn = 3m;
+            Assert.AreEqual(expectedReturn,returnMoney.ReturnMoney(3m));
+        }
+    }
+}
