82f26d6b9bcb22ddafd70c03bbf97fb27b2fad79
Claire Kirby <claire.kirby@gmail.com>
2020-06-06 15:06:05 -0400

    updated purchase item menu

--- Capstone/Menu.cs
+++ Capstone/Menu.cs
@@ -120,12 +120,17 @@ namespace Capstone
                 {                    
                     vendoMatic.DispenseItem(enteredItemID);
                     vendoMatic.DispenseItemPrintOut(enteredItemID, currentBalance);
+                    currentBalance = currentBalance - selectedItem.ItemPrice;
                 }
-                else
+                else if (currentBalance < selectedItem.ItemPrice)
                 {
                     Console.WriteLine("You do not have enough money to make that purchase. Please input money.");
                 }
-                currentBalance = currentBalance - selectedItem.ItemPrice;
+                else
+                {
+                    
+                }
+                
                 PurchaseItems();
 
             }
